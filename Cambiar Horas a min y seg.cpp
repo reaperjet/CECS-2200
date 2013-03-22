2e9360a471e255e2e585af677c84114ab6d13eb7
Joet Rivera <jet_629@hotmail.com>
2013-03-22 15:42:43 -0400

    added files

---
+++ Cambiar Horas a min y seg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace::std;
+//Programa para convertir de horas a minutos y a segundos
+//Cambiar horas a min y seg
+//Joet Rivera ID: 81002
+//CECS 2200 Sec. 09
+void main(){
+	int horas;
+	cout<<"Entre las horas";
+	cin>>horas;
+	minutos=horas*60;
+		cout<<"La convercion a minutos es"minutos"minutos";
+	segundos=minutos*60;
+		cout<<"La convercion a segundos es"segundos"segundos";
+	system("pause");
+}//end main
